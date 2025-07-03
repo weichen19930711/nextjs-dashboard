d6f5c28ed21c3f8856ef5586c56de573ba520407
h_time <2471142690@qq.com>
2025-07-03 21:41:34 +0800

    feat：test

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -18,25 +18,6 @@ export default async function Page() {
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
-      <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardsSkeleton />}>
-          <CardWrapper />
-        </Suspense>
-      </div>
-      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* use Suspense to stream only this component */}
-        <Suspense fallback={<RevenueChartSkeleton />}>
-          <RevenueChart />
-        </Suspense>
-        {/**
-         * How you want the user to experience the page as it streams.
-         * What content you want to prioritize.
-         * If the components rely on data fetching.
-         */}
-        <Suspense fallback={<LatestInvoicesSkeleton />}>
-          <LatestInvoices />
-        </Suspense>
-      </div>
     </main>
   );
 }
